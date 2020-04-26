05ca2005e8487824deb32e793a0ffdbdd2ede9b0
arieansyah <arieansyahp.bun@gmail.com>
2020-04-27 02:10:24 +0700

    debug repository auth

--- app/Repositories/Auth/AuthRepository.php
+++ app/Repositories/Auth/AuthRepository.php
@@ -3,6 +3,9 @@
 namespace App\Repositories\Auth;
 
 use App\Models\User;
+use App\Events\User\UserCreated;
+use Illuminate\Support\Facades\DB;
+use App\Exceptions\GeneralException;
 use App\Repositories\BaseRepository;
 use Illuminate\Support\Facades\Hash;
 
@@ -15,12 +18,19 @@ class AuthRepository extends BaseRepository
 
     public function create(array $data): User
     {
-        $user = $this->model::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
+        return DB::transaction(function () use ($data) {
+            $user = $this->model::create([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'password' => Hash::make($data['password']),
+            ]);
 
-        return $user;
+            if ($user) {
+                event(new UserCreated($user));
+                return $user;
+            }
+        });
+
+        throw new GeneralException(__('exceptions.users.create_error'));
     }
 }
