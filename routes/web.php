68c11c18159ed89acdd190548f5f0c9dc0a5e580
arieansyah <arieansyahp.bun@gmail.com>
2020-04-27 01:40:06 +0700

    add route register and post with slug

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
+Route::get('/', 'PostController@index');
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::post('register', 'AuthController@store');
+Route::get('register', 'AuthController@index')->name('register');
+
 Route::get('/post', 'PostController@index');
+Route::get('/post/{post:slug}', 'PostController@show')->name('post.show');
+
