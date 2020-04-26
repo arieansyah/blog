083f0ed55170e32e5b888525d797d95d1465cbb8
arieansyah <arieansyahp.bun@gmail.com>
2020-04-27 01:41:18 +0700

    debug post

--- resources/views/posts/_show.blade.php
+++ resources/views/posts/_show.blade.php
@@ -1,13 +1,13 @@
 <div class="card">
     <div class="card-body">
-        <h4 v-pre class="card-title">{{ $post->title }}</h4>
+        <a href="{{ route('post.show', $post->slug) }}"><h4 v-pre class="card-title">{{ $post->title }}</h4></a>
 
         <p class="card-text">{{ $post->content }}</p>
 
         <p class="card-text">
-            <small class="text-muted">{{ $post->user->email }}</small><br>
-            <small class="text-muted">
-                <i class="fa fa-comments-o" aria-hidden="true"></i> {{ $post->user->name }}
+            <small class="text-muted">{{ $post->user->email }} || {{ $post->user->name }}</small><br>
+            <small class="text-muted float-right">
+                <i class="fa fa-comments-o" aria-hidden="true"></i> {{ count($post->comment) }}
             </small>
         </p>
     </div>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 
+  <h1>Blog</h1>
   {{-- maybe, sometimes we can create search or anything else in here --}}
 
   @include ('posts/_list')
