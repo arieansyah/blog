14909cf62de43fdfd5065c70349f211fe1f4d8d4
arieansyah <arieansyahp.bun@gmail.com>
2020-04-26 14:07:11 +0700

    add layout posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $post = Post::with('user')->get();
+        // dd($post);
+        return view('posts.index')
+            ->with('post', $post);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use App\Models\Comment;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,5 +11,10 @@ class Post extends Model
     public function comment()
     {
         return $this->hasMany(Comment::class, 'post_id', 'id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
     }
 }
