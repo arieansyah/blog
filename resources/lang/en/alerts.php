f47e43d0e588da4bec1b893323d598c0038450f8
arieansyah <arieansyahp.bun@gmail.com>
2020-04-27 01:36:56 +0700

    add alert on lang

---
+++ resources/lang/en/alerts.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Alert Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain alert messages for various scenarios
+    | during CRUD operations. You are free to modify these language lines
+    | according to your application's requirements.
+    |
+    */
+
+    'users' => [
+        'created' => 'The user was successfully created.',
+    ],
+];
