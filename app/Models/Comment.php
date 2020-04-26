4767164a02625355630dff01677e2cb98f7005d6
arieansyah <arieansyahp.bun@gmail.com>
2020-04-27 01:42:10 +0700

    add relation on model comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,12 +3,19 @@
 namespace App\Models;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+
     public function post()
     {
         return $this->belongsTo(Post::class, 'user_id', 'id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'email', 'email');
     }
 }
